22c100a363dd0bce1d3e208468b0cf3c3d133e93
Luna <69774917+carlyjb17@users.noreply.github.com>
2023-04-14 11:57:13 +0000

    aa

---
+++ ft_strcmp.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+int ft_strcmp(char *s1, char *s2)
+{
+    while (*s1 || *s2)
+    {
+        if (*(unsigned char *)s1 != *(unsigned char *)s2)
+            return (*(unsigned char *)s1 - *(unsigned char *)s2);
+        s1++;
+        s2++;
+    }
+    return (0);
+}
+
+int main()
+{
+    printf("%i", ft_strcmp("holo", "hola"));
+}
